037fcf4ddba5644d6518781d97fbbeb38d21d526
Vahid Mohammadi <email@vmoh.ir>
2018-05-30 23:37:43 +0430

    not important

--- index.php
+++ index.php
@@ -46,28 +46,36 @@ function log_debug($data, $chat_id = 92454)
         'text' => $text,
     ]);
 }
+function rstr($str, $removeText)
+{
+    return str_ireplace($removeText, '', $str);
+}
+function rstrings($str, $rstrings)
+{
+    for ($i = 0; $i < count($rstrings); $i++) {
+        $str = rstr($str, $rstrings[0]);
+    }
+    return $str;
+}
+function titleFunction($title)
+{
+    return trim(rstrings($title, ['FREE DOWNLOAD', '[FREE DOWNLOAD]', '[ORIGINAL MIX]', '[EXTENDED MIX]', 'ORIGINAL MIX', 'EXTENDED MIX']));
+}
 
 if ($message != null) {
     $chat = $message->getChat();
     $chat_id = (int) $chat->getId();
-    // $text       = $message->getText();
     $audio = $message->getAudio();
     $message_id = $message->getMessageId();
-    // $username   = $message->getFrom()->getUsername();
-    // $user       = $message->getFrom();
-    // $username   = $user->getUsername();
-    // $fullname   = $user->getFirstName() . ' ' . $user->getLastName();
     try {
         if ($audio) {
-            log_debug($audio);
-            // $file_url = get_file_link($audio->fileId);
-            // log_debug($file_url);
+            // log_debug($audio);
             $fileSizeString = round(($audio->fileSize / 1024 / 1024), 2) . 'MB';
 
-            $musicStr = ($audio->title ? '🎧 Music: ' . $audio->title : '🎧 Music');
-            $performerStr = ($audio->performer ? '👤 By: ' . $audio->performer : '👤');
-            $durationStr = ($audio->duration ? '🕒 Duration: ' . ((int) floor($audio->duration / 60)) . ':' . ($audio->duration % 60) : '🕒');
-            $sizeStr = '💾 Size: ' . $fileSizeString;
+            $musicStr = ($audio->title ? '🎧 `Music:` ' . titleFunction($audio->title) : '`🎧 Music`');
+            $performerStr = ($audio->performer ? '👤 `By:` ' . $audio->performer : '`👤 By:` Unknown');
+            $durationStr = '🕒 `Duration:` ' . ((int) floor($audio->duration / 60)) . ':' . ($audio->duration % 60);
+            $sizeStr = '💾 `Size:` ' . $fileSizeString;
             $idStr = '🆔 @edmusics';
 
             $caption = $musicStr . PHP_EOL . $performerStr . PHP_EOL . $durationStr . PHP_EOL . $sizeStr . PHP_EOL . $idStr;
@@ -86,6 +94,7 @@ if ($message != null) {
                 'chat_id' => $channel_id,
                 'audio' => $audio->fileId,
                 'caption' => $caption,
+                'parse_mode' => 'markdown',
             ]);
         }
 
